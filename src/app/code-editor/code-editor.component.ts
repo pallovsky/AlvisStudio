7c85b49083a1d92001a46bcdcf530716376cb314
Krzysztof Pala <pallovsky@gmail.com>
2023-04-23 18:14:42 +0200

    adding code editor

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,12 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import * as ace from "ace-builds";
 
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.scss']
 })
-export class CodeEditorComponent implements OnInit {
+export class CodeEditorComponent implements AfterViewInit {
 
-  ngOnInit(): void {
+  @ViewChild("editor") private editor: ElementRef<HTMLElement> | undefined;
+
+  ngAfterViewInit(): void {
+    ace.config.set("fontSize", "14px");
+
+    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
+
+    const aceEditor = ace.edit(this.editor!.nativeElement);
+
+    aceEditor.session.setValue("" +
+      "agent A {\n" +
+      "  x :: Int = 5;\n" +
+      "  out q x;\n" +
+      "}\n" +
+      "\n" +
+      "agent B {\n" +
+      "  x :: Int = 0;\n" +
+      "  proc p1 { in p1 x; exit; }\n" +
+      "  proc p2 { in p2 x; exit; }\n" +
+      "}");
+
+    aceEditor.setTheme('ace/theme/twilight');
+    aceEditor.session.setMode('ace/mode/haskell');
   }
 }
