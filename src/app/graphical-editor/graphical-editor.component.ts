819c0e6b386d9babfc8800549843a49687f86c9c
Krzysztof Pala <pallovsky@gmail.com>
2023-06-08 12:25:24 +0200

    removing go js

--- src/app/graphical-editor/graphical-editor.component.ts
+++ src/app/graphical-editor/graphical-editor.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import * as go from 'gojs'
 
 @Component({
   selector: 'app-graphical-editor',
@@ -8,25 +7,7 @@ import * as go from 'gojs'
   encapsulation: ViewEncapsulation.None
 })
 export class GraphicalEditorComponent implements OnInit {
-  public model: go.GraphLinksModel = new go.GraphLinksModel();
 
   ngOnInit(): void {
-    this.model = new go.GraphLinksModel()
-    this.model.linkFromPortIdProperty = "fromPort";
-    this.model.linkToPortIdProperty = "toPort";
-    this.model.nodeDataArray =
-      [
-        { key: "A", color: "lightgreen", figure: "RoundedRectangle" },
-        { key: "B", color: "lightgreen", figure: "RoundedRectangle" },
-        { key: "C", color: "darkgreen", figure: "Rectangle" },
-        { key: "D", color: "darkgreen", figure: "Rectangle" }
-      ];
-    this.model.linkDataArray =
-      [
-        { from: "A", to: "B" },
-        { from: "C", to: "D" },
-        { from: "D", to: "A" }
-      ];
   }
-
 }
